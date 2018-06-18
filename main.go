d9a9f44cfc6c9c5fe28ced7940effdb2fb8621af
slehkyi <s.lehkyi@cronostelecom.com>
2018-06-18 16:44:08 +0200

    server works and makes checks

--- main.go
+++ main.go
@@ -10,17 +10,67 @@ import (
 )
 
 var AmActive = false
+const srv1 = "http://127.0.0.1:8080/check"
+const srv2 = "http://127.0.0.1:8088/check"
 
 func main() {
 
-	var result responseOK
+	go Change(nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/check", CheckHandler)
+	go http.ListenAndServe(":8080", mux)
 
-	resp, err := http.Get("http://127.0.0.1:8088/check")
+	for {
+		time.Sleep(2 * time.Second)
+		go Change(mux)
+	}
+}
+
+func Change(mux *http.ServeMux) {
+	result, err := Check(srv2)
 	if err != nil {
-		http.ListenAndServe(":8080", nil)
 		Active(true)
 		fmt.Println("I am Active")
 		fmt.Println(err)
+		mux = http.NewServeMux()
+		mux.HandleFunc("/check", CheckHandler)
+		http.ListenAndServe(":8080", mux)
+	}
+	if result.Active == false {
+		fmt.Println("I am Active")
+		Active(true)
+		fmt.Println(AmActive)
+	} else {
+		fmt.Println("I am not active, but listening")
+		Active(false)
+		fmt.Println(AmActive)
+	}
+}
+
+type responseOK struct {
+	Active bool
+	TimeStamp int64
+}
+
+func CheckHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	bodyActive := responseOK{
+		Active: AmActive,
+		TimeStamp: time.Now().UnixNano(),
+	}
+	j, err := json.Marshal(bodyActive)
+	if err != nil {
+		log.Println(err)
+	}
+	res.Write(j)
+}
+
+func Check(url string) (responseOK, error) {
+	var result responseOK
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return responseOK{}, err
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
@@ -34,27 +84,7 @@ func main() {
 	}
 	fmt.Println(result)
 
-	http.HandleFunc("/check", Check)
-}
-
-type responseOK struct {
-	status string
-	timeStamp int64
-}
-
-func Check(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set("Content-Type", "application/json")
-	body := responseOK{
-		status: "OK",
-		timeStamp: time.Now().UnixNano(),
-	}
-	if AmActive == true {
-		j, err := json.Marshal(body)
-		if err != nil {
-			log.Println(err)
-		}
-		res.Write(j)
-	}
+	return result, nil
 }
 
 func Active(val bool) {
